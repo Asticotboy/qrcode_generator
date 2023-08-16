51bf515b67371c02eddfa3c2ff848b439dec6e88
Asticotboy <c.ludwigs@outlook.fr>
2023-08-16 09:29:40 +0200

    Update

---
+++ src/js/script.js
@@ -0,0 +1,72 @@
+const form = document.getElementById("generate-form");
+const qr = document.getElementById("qrcode");
+
+// Button submit
+const onGenerateSubmit = (e) => {
+  e.preventDefault();
+
+  clearUI();
+
+  const url = document.getElementById("url").value;
+  const size = document.getElementById("size").value;
+
+  if (url === "") {
+    alert("Please enter a URL");
+  } else {
+    showLoadingAnimation();
+    
+    setTimeout(() => {
+      hideLoadingAnimation();
+      generateQRCode(url, size);
+
+      setTimeout(() => {
+        const saveUrl = qr.querySelector("img").src;
+        createSaveBtn(saveUrl);
+      }, 50);
+    }, 1000);
+  }
+};
+
+// Generate QR code
+const generateQRCode = (url, size) => {
+  const qrcode = new QRCode("qrcode", {
+    text: url,
+    width: size,
+    height: size,
+  });
+};
+
+// Clear QR code and save button
+const clearUI = () => {
+  qr.innerHTML = "";
+  const saveBtn = document.getElementById("save-link");
+  if (saveBtn) {
+    saveBtn.remove();
+  }
+};
+
+const showLoadingAnimation = () => {
+  const spinner = document.getElementById("spinner");
+  spinner.style.display = "block";
+};
+
+const hideLoadingAnimation = () => {
+  const spinner = document.getElementById("spinner");
+  spinner.style.display = "none";
+};
+
+// Create save button to download QR code as image
+const createSaveBtn = (saveUrl) => {
+  const link = document.createElement("a");
+  link.id = "save-link";
+  link.className =
+    "bg-purple-500 hover:bg-black text-white font-bold py-2 rounded w-1/3 m-auto my-5";
+  link.href = saveUrl;
+  link.download = "qrcode";
+  link.innerHTML = "Save Image";
+  document.getElementById("generated").appendChild(link);
+};
+
+hideLoadingAnimation();
+
+form.addEventListener("submit", onGenerateSubmit);
